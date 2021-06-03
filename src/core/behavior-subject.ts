2b59f2250365606aae4c83acb8487c19e8683c5b
tanbo <464328895@qq.com>
2021-06-04 00:44:34 +0800

    fix: 修复 behavior subject 默认值的问题

--- src/core/behavior-subject.ts
+++ src/core/behavior-subject.ts
@@ -7,6 +7,11 @@ export class BehaviorSubject<T> extends Subject<T> {
     super()
   }
 
+  next(newValue: T) {
+    this.defaultValue = newValue;
+    super.next(newValue);
+  }
+
   subscribe(observer: PartialObserver<T>): Subscription;
   subscribe(observer: ((value: T) => void), error?: (err: any) => void, complete?: () => void): Subscription;
   subscribe(observer: any, error?: any, complete?: any): Subscription {
