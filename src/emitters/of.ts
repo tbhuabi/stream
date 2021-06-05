8aa70c100d8dfb7193febef882aabb995f91f780
tanbo <464328895@qq.com>
2021-06-05 17:31:36 +0800

    feat: 完善类型

--- src/emitters/of.ts
+++ src/emitters/of.ts
@@ -12,6 +12,7 @@ export function of<T1, T2, T3, T4, T5, T6>(s1: T1, s2: T2, s3: T3, s4: T4, s5: T
 export function of<T1, T2, T3, T4, T5, T6, T7>(s1: T1, s2: T2, s3: T3, s4: T4, s5: T5, s6: T6, s7: T7): Stream<T1 | T2 | T3 | T4 | T5 | T6 | T7>;
 export function of<T1, T2, T3, T4, T5, T6, T7, T8>(s1: T1, s2: T2, s3: T3, s4: T4, s5: T5, s6: T6, s7: T7, s8: T8): Stream<T1 | T2 | T3 | T4 | T5 | T6 | T7 | T8>;
 export function of<T1, T2, T3, T4, T5, T6, T7, T8, T9>(s1: T1, s2: T2, s3: T3, s4: T4, s5: T5, s6: T6, s7: T7, s8: T8, s9: T9): Stream<T1 | T2 | T3 | T4 | T5 | T6 | T7 | T8 | T9>;
+export function of<T>(...data: T[]): Stream<T>;
 export function of(...data: any[]): Stream<any>;
 export function of<T>(...data: T[]): Stream<T> {
   return new Stream<T>(observer => {
--- src/emitters/race.ts
+++ src/emitters/race.ts
@@ -13,6 +13,7 @@ export function race<T1, T2, T3, T4, T5, T6>(s1: Stream<T1>, s2: Stream<T2>, s3:
 export function race<T1, T2, T3, T4, T5, T6, T7>(s1: Stream<T1>, s2: Stream<T2>, s3: Stream<T3>, s4: Stream<T4>, s5: Stream<T5>, s6: Stream<T6>, s7: Stream<T7>): Stream<T1 | T2 | T3 | T4 | T5 | T6 | T7>;
 export function race<T1, T2, T3, T4, T5, T6, T7, T8>(s1: Stream<T1>, s2: Stream<T2>, s3: Stream<T3>, s4: Stream<T4>, s5: Stream<T5>, s6: Stream<T6>, s7: Stream<T7>, s8: Stream<T8>): Stream<T1 | T2 | T3 | T4 | T5 | T6 | T7 | T8>;
 export function race<T1, T2, T3, T4, T5, T6, T7, T8, T9>(s1: Stream<T1>, s2: Stream<T2>, s3: Stream<T3>, s4: Stream<T4>, s5: Stream<T5>, s6: Stream<T6>, s7: Stream<T7>, s8: Stream<T8>, s9: Stream<T9>): Stream<T1 | T2 | T3 | T4 | T5 | T6 | T7 | T8 | T9>;
+export function race<T>(...inputs: Stream<T>[]): Stream<T>;
 export function race(...inputs: Stream<any>[]): Stream<any>;
 export function race<T>(...inputs: Stream<T>[]): Stream<T> {
   return new Stream<T>(observer => {
