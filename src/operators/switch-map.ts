26edb620b87d4afde89cdc34be743d3448e16797
tanbo <464328895@qq.com>
2021-06-22 22:11:00 +0800

    fix: 修复 switchMap 订阅未取消的问题

--- src/operators/switch-map.ts
+++ src/operators/switch-map.ts
@@ -1,4 +1,4 @@
-import { Observable, Operator, PartialObserver } from '../core/_api'
+import { Observable, Operator, PartialObserver, Subscription } from '../core/_api'
 
 /**
  * 返回一个新的数据流，并以新数据流的订阅结果，发送出去
@@ -7,9 +7,13 @@ import { Observable, Operator, PartialObserver } from '../core/_api'
 export function switchMap<T, U>(handle: (value: T) => Observable<U>): Operator<T, U> {
   return function (source: Observable<T>) {
     return new Observable<U>(subscriber => {
+      let prev: Subscription;
       const obs: PartialObserver<T> = {
         next(value: T) {
-          handle(value).subscribe(value2 => {
+          if (prev) {
+            prev.unsubscribe();
+          }
+          prev = handle(value).subscribe(value2 => {
             subscriber.next(value2);
           }, function (err) {
             obs.error(err)
