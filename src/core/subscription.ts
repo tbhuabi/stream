4dbf66a2e750ab698acc7799f6c3305800025671
tanbo <tanbo@cmcm.com>
2021-10-22 18:47:50 +0800

    feat: 合并分支

--- src/core/subscription.ts
+++ src/core/subscription.ts
@@ -1,4 +1,23 @@
 export class Subscription {
-  constructor(public unsubscribe: () => void) {
+  private subs: Subscription[] = [];
+
+  private isStopped = false;
+
+  constructor(private unsubscribeCallback?: () => void) {
+  }
+
+  add(subscription: Subscription) {
+    if (this.isStopped) {
+      return;
+    }
+    this.subs.push(subscription);
+  }
+
+  unsubscribe() {
+    this.isStopped = true;
+    if (this.unsubscribeCallback) {
+      this.unsubscribeCallback();
+    }
+    this.subs.forEach(i => i.unsubscribe());
   }
 }
