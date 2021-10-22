ebf9f8620de81a8107b6d3d50ca1b3996d8b5b63
tanbo <tanbo@cmcm.com>
2021-10-22 18:51:41 +0800

    feat: 合并分支

--- src/operators/tap.ts
+++ src/operators/tap.ts
@@ -4,12 +4,12 @@ import { Observable, Operator } from '../core/_api'
  * 在数据流中添加副作用
  * @param callback 副作用函数
  */
-export function tap<T>(callback: () => void): Operator<T, T> {
+export function tap<T>(callback: (value: T) => void): Operator<T, T> {
   return function (source: Observable<T>) {
     return new Observable<T>(subscriber => {
       return source.subscribe({
         next(v: T) {
-          callback()
+          callback(v)
           subscriber.next(v);
         },
         error(err?: Error) {
