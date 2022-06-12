14905bb0196c50bc2296b746e3340ecfa5b44a2c
tanbo <464328895@qq.com>
2022-06-13 02:37:19 +0800

    feat: 优化命名

--- src/observables/throw-error.ts
+++ src/observables/throw-error.ts
@@ -4,7 +4,7 @@ import { Observable } from '../core/_api';
  * 创建一个发出错误的可观察对象
  * @param err
  */
-export function error<T extends Error>(err?: T) {
+export function throwError<T extends Error>(err?: T) {
   return new Observable(subscriber => {
     subscriber.error(err || new Error())
   })
