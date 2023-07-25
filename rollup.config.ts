65956d3f1b35804493c1966889c3b292a04b5c6a
tbhuabi <464328895@qq.com>
2023-07-25 20:43:11 +0800

    chore: 更改构建方式

---
+++ rollup.config.ts
@@ -0,0 +1,22 @@
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+  input: './src/public-api.ts',
+  output: [
+    {
+      file: './bundles/index.js',
+      format: 'cjs'
+    },
+    {
+      file: './bundles/index.esm.js',
+      format: 'esm'
+    }
+  ],
+  plugins: [
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig-build.json'
+    })
+  ]
+}
